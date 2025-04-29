2bbd240663bafd19fc025e591c9c8b95890f8ee5
roxblnfk <roxblnfk@ya.ru>
2025-04-29 11:26:02 +0400

    Fix versions, update metafiles

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,19 +2,10 @@
 
 declare(strict_types=1);
 
-if (!file_exists(__DIR__.'/src')) {
-    exit(0);
-}
+require_once 'vendor/autoload.php';
 
-return (new PhpCsFixer\Config())
-    ->setRules([
-        '@PSR12' => true,
-        'ternary_operator_spaces' => false,
-    ])
-    ->setRiskyAllowed(true)
-    ->setFinder(
-        (new PhpCsFixer\Finder())
-            ->in(__DIR__.'/src')
-            ->append([__FILE__])
-    )
-    ->setCacheFile('.php-cs-fixer.cache');
+return \Spiral\CodeStyle\Builder::create()
+    ->include(__DIR__ . '/src')
+    ->include(__DIR__ . '/tests')
+    ->include(__FILE__)
+    ->build();
